99234a711bc7522dfe9209a5f06bce2da78a4bec
beecomci <hkwoo7788@hanmail.net>
2017-03-04 17:33:13 +0900

    HandlerMethodArgumentResolver 적용

--- first/src/main/java/first/common/dao/AbstractDAO.java
+++ first/src/main/java/first/common/dao/AbstractDAO.java
@@ -7,9 +7,11 @@ import org.apache.commons.logging.LogFactory;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 
+//쿼리는 sqlSession 메소드를 호출하면 되는데 앞으로 개발할때 편하게 로그를 남기기위해 insert, update, delete, select 메소드 재정의
 public class AbstractDAO {
 	protected Log log = LogFactory.getLog(AbstractDAO.class);
 	
+	//어노테이션을 통해 context-mapper.xml에 선언했던 의존관계를 자동으로 주입
 	@Autowired
 	private SqlSessionTemplate sqlSession;
 	
